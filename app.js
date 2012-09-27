d0e1fa10cd18be4fe1fa53ae7b4dbd6ea4927064
Colin Wood <colinjameswood@gmail.com>
2012-09-27 08:14:33 -0500

    Comments work, but this commit is in "staging" mode.

--- app.js
+++ app.js
@@ -54,6 +54,10 @@ var imgur       = require('./lib/imgur.js')
   , storyLoopID = null
   , mirrorLoopID = null
   , commentLoopID = null
+  
+    // used to only print "all stories mirrored/commented" once, until there are more stories in the queue
+  , printAllMirrored = true
+  , printAllCommented = true
 ;
 
 console.beep = function(){
@@ -72,8 +76,8 @@ reddit.login({
 		console.log("Logged in.");
 		storyLoop();
 		storyLoopID = setInterval(storyLoop, 5000 * 60); // check new stories every 5 minutes
-		//mirrorLoopID = setInterval(mirrorLoop, 1000 * 60); // mirror a link every minute
-		//commentLoopID = setInterval(commentLoop, 10000 * 60); // comment on a link every 10 minutes
+		mirrorLoopID = setInterval(mirrorLoop, 1000 * 60); // mirror a link every minute
+		commentLoopID = setInterval(commentLoop, 10000 * 60); // comment on a link every 10 minutes
 	}
 });
 
@@ -98,21 +102,21 @@ function storyLoop() {
 			
 			var oldStoryQueueSize = StoryQueue.size();
 			
-			StoryQueue.add(stories).save({
-				success: function(){
-					
-					var numAddedStories = StoryQueue.size() - oldStoryQueueSize;
-					
-					console.log("    Added " + numAddedStories + " new " + ((numAddedStories==1)?"story.":"stories."));
-					
-					if(!mirrorLoopID){
-						// mirror a link every minute
-						mirrorLoop();
-						mirrorLoopID = setInterval(mirrorLoop, 1000 * 60);
-					}
-					
-				}
-			});
+			StoryQueue.add(stories);
+			
+			var numAddedStories = StoryQueue.size() - oldStoryQueueSize;
+			
+			if(numAddedStories > 0){
+				StoryQueue.save();
+			}
+				
+			console.log("    Added " + numAddedStories + " new " + ((numAddedStories==1)?"story.":"stories."));
+			
+			//	if(!mirrorLoopID){
+			//		// mirror a link every minute
+			//		mirrorLoop();
+			//		mirrorLoopID = setInterval(mirrorLoop, 1000 * 60);
+			//	}
 			
 		}
 	});
@@ -125,9 +129,14 @@ function mirrorLoop() {
 	var story = StoryQueue.find(function(story){ return story.get("mirrored_image") === null; });
 	
 	if(!story) {
-		console.log("All stories in queue have been mirrored!");
+		if(printAllMirrored){
+			console.log("All stories in queue have been mirrored!");
+			printAllMirrored = false;
+		}
 		return true;
 	}
+	
+	printAllMirrored = true;
 	
 	deviantart.getImage(story.get("url"), function(deviantart_image){
 		
@@ -135,7 +144,7 @@ function mirrorLoop() {
 			
 			// there was an error mirroring this image, abort
 			// and move it down the queue (handled internally)
-			if(!deviantart_image || mirrored_image == "http://imgur.com/?error") {
+			if(!deviantart_image || !mirrored_image || mirrored_image == "http://imgur.com/?error") {
 				
 				console.log("Failed to mirror image:");
 				console.log('    "' + story.get("title") + '" (http://reddit.com' + story.get("permalink") + ')');
@@ -151,23 +160,19 @@ function mirrorLoop() {
 				"mirrored_image": mirrored_image
 			});
 			
-			StoryQueue.save({
-				success: function(){
-					
-					console.log("Mirrored image:");
-					console.log('    "' + story.get("title") + '" (http://reddit.com' + story.get("permalink") + ')');
-					console.log('    -> ' + story.get("url"));
-					console.log('    -> ' + story.get("deviantart_image"));
-					console.log('    -> ' + story.get("mirrored_image"));
-					
-					if(!commentLoopID){
-						// comment on a link every 10 minutes
-						commentLoop();
-						commentLoopID = setInterval(commentLoop, 10000 * 60);
-					}
-					
-				}
-			});
+			console.log("Mirrored image:");
+			console.log('    "' + story.get("title") + '" (http://reddit.com' + story.get("permalink") + ')');
+			console.log('    -> ' + story.get("url"));
+			console.log('    -> ' + story.get("deviantart_image"));
+			console.log('    -> ' + story.get("mirrored_image"));
+			
+			//	if(!commentLoopID){
+			//		// comment on a link every 10 minutes
+			//		commentLoop();
+			//		commentLoopID = setInterval(commentLoop, 10000 * 60);
+			//	}
+			
+			StoryQueue.save();
 			
 		});
 		
@@ -177,18 +182,36 @@ function mirrorLoop() {
 function commentLoop() {
 	
 	// find the first story that hasn't been commented on yet
-	var story = StoryQueue.find(function(story){ return story.get("comment_id") === null; });
+	var story = StoryQueue.find(function(story){
+		return story.get("mirrored_image") !== null && story.get("comment_id") === null;
+	});
 	
 	if(!story) {
-		console.log("All stories in queue have been commented on!");
+		if(printAllCommented){
+			console.log("All mirrored stories in queue have been commented on!");
+			printAllCommented = false;
+		}
 		return true;
 	}
+	
+	printAllCommented = true;
 	
 	// put Scootaloo in the comment if we're on MLP :D
 	var pony = (story.get("subreddit").toLowerCase() == "mylittlepony")?"[](/scootacheer)":"",
 	    
 	    commentText = pony + "[Here's an Imgur mirror!](" + story.get("mirrored_image") + ")\n- - -\n^I ^am ^a ^bot. ^| [^FAQ](http://www.reddit.com/r/DeviantArtMirrorBot/comments/10cupp/faq/) ^| [^Report ^a ^Problem](http://www.reddit.com/r/DeviantArtMirrorBot/submit?title=Problem%20Report%26text=Describe%20the%20problem%20here.) ^| [^Contact ^the ^Creator](http://www.reddit.com/message/compose/?to=Anaphase%26subject=ATTN:%20DeviantArtMirrorBot)";
 	
+	story.set({"comment_id": "fake_id"});
+	
+	console.beep();
+	console.log("I WOULD HAVE Commented on story:");
+	console.log('    "' + story.get("title") + '" (http://reddit.com' + story.get("permalink") + ')');
+	console.log('    -> ' + story.get("url"));
+	console.log('    -> Comment ID: ' + "fake_id");
+	
+	return true;
+	
+	/*
 	reddit.comment({
 		thingId: story.get("id"),
 		thingType: "link",
@@ -223,5 +246,6 @@ function commentLoop() {
 			
 		}
 	});
+	*/
 	
 }
