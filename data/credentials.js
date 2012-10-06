ca9e57d2b2a182266d299a2376ceec9445ac90e9
Colin Wood <colinjameswood@gmail.com>
2012-10-06 16:02:02 -0500

    Moved credentials file.

---
+++ data/credentials.js
@@ -0,0 +1,20 @@
+module.exports = {
+    
+    reddit: {
+        username: ""
+      , password: ""
+    }
+    
+  , imgur: {
+        key: ""
+      , secret: ""
+      , access_token: ""
+      , access_token_secret: ""
+    }
+    
+  , bitly: {
+      username: ""
+    , apikey: ""
+  }
+    
+}
