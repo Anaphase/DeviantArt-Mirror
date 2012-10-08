2043e1d3760fa032a1dd80452f28ed77d4fdb91e
Colin Wood <colinjameswood@gmail.com>
2012-10-07 23:06:10 -0500

    Better initial loop calling (staggered.)

--- app.js
+++ app.js
@@ -8,25 +8,27 @@ console.log("DeviantArt Mirror Bot - By http://reddit.com/u/Anaphase".underline.
 // login and do once cycle of commenting before starting loops
 damb.login(function(){
     damb.getStories(function(){
-        damb.processStory(function(){
-            
-            // get new stories every 5 minutes
-            setInterval(function(){ damb.getStories() }, 1000 * 60 * 5)
-            
+        damb.getDeviantArtData(function(){
+            damb.shortenUrl(function(){
+                damb.watermark(function(){
+                    damb.mirror(function(){
+                        damb.comment(function(){
+                            // comment every 2 minutes
+                            setInterval(function(){ damb.comment() }, 2000 * 60)
+                        })
+                        // mirror every minute
+                        setInterval(function(){ damb.mirror() }, 1000 * 60)
+                    })
+                    // watermark every minute
+                    setInterval(function(){ damb.watermark() }, 1000 * 60)
+                })
+                // shorten a link every 30 seconds
+                setInterval(function(){ damb.shortenUrl() }, 1000 * 30)
+            })
             // get DA data 30 seconds
             setInterval(function(){ damb.getDeviantArtData() }, 1000 * 30)
-            
-            // shorten a link every 30 seconds
-            setInterval(function(){ damb.shortenUrl() }, 1000 * 30)
-            
-            // watermark every 10 seconds
-            setInterval(function(){ damb.watermark() }, 1000 * 10)
-            
-            // mirror every minute
-            setInterval(function(){ damb.mirror() }, 1000 * 60)
-            
-            // comment every minute
-            setInterval(function(){ damb.comment() }, 1000 * 60)
         })
+        // get new stories every 10 minutes
+        setInterval(function(){ damb.getStories() }, 1000 * 60 * 10)
     })
 })
