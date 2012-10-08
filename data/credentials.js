235c662bf022b2b09c1ac741cb7bca4e11dedede
Colin Wood <colinjameswood@gmail.com>
2012-10-08 16:51:20 -0500

    credentials

--- data/credentials.js
+++ data/credentials.js
@@ -1,20 +1,20 @@
 module.exports = {
     
     reddit: {
-        username: "DeviantArtMirrorBot"
-      , password: "poopie"
+        username: ""
+      , password: ""
     }
     
   , imgur: {
-        key: "b7d8ac2fb0c94cc0a2c7c7775e74ef95050690f27"
-      , secret: "afbad89accda4a7924b9573ed3e26c30"
-      , access_token: "c31a78e33e5ebce95d4fdd923790b230050691c29"
-      , access_token_secret: "382f0dcd5d01e487c3e37ac9805fe69e"
+        key: ""
+      , secret: ""
+      , access_token: ""
+      , access_token_secret: ""
     }
     
   , bitly: {
-      username: "deviantartmirrorbot"
-    , apikey: "R_177c6ea3caa55c3d295b8c3c18075ceb"
+      username: ""
+    , apikey: ""
   }
     
 }
