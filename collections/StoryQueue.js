5851f559ebde47e6599a649f85d9998bfbd416e5
Colin Wood <colinjameswood@gmail.com>
2012-10-07 20:14:15 -0500

    Removed unused functions after redesign of DAMB

--- collections/StoryQueue.js
+++ collections/StoryQueue.js
@@ -55,44 +55,6 @@ module.exports = Backbone.Collection.extend({
         return this.where({"ignore": false}).length
     }
     
-  , numMirrored: function(){
-        return this.filter(function(story){
-            return story.get("imgur_image") !== null && !story.get("ignore")
-        }).length
-    }
-    
-  , numCommented: function(){
-        return this.filter(function(story){
-            return story.get("comment_id") !== null && !story.get("ignore")
-        }).length
-    }
-    
-  , numNotMirrored: function(){
-        return this.filter(function(story){
-            return story.get("imgur_image") === null && !story.get("ignore")
-        }).length
-    }
-    
-  , numNotCommented: function(){
-        return this.filter(function(story){
-            return story.get("comment_id") === null && !story.get("ignore")
-        }).length
-    }
-    
-    // find the first story that hasn't been mirrored yet
-  , getNextStoryForMirror: function(){
-        return this.find(function(story){
-            return story.get("imgur_image") === null && !story.get("ignore")
-        })
-    }
-    
-    // find the first story that has been mirrored, but hasn't been commented on yet
-  , getNextStoryForComment: function(){
-        return this.find(function(story){
-            return story.get("imgur_image") !== null && story.get("comment_id") === null && !story.get("ignore")
-        })
-    }
-    
   , load: function(){
         this.reset(JSON.parse(fs.readFileSync('./data/StoryQueue.json')))
     }
